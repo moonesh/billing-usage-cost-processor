ab9a95e04e2c9b1c8b73bb356970c537449fe23d
moonesh <mooneshkachroo@hotmail.com>
2020-01-16 17:40:02 +0530

    First Commit - SDF Processor

---
+++ src/main/java/com/billing/usage/cost/processor/UsageCostProcessorApplication.java
@@ -0,0 +1,13 @@
+package com.billing.usage.cost.processor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsageCostProcessorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UsageCostProcessorApplication.class, args);
+	}
+
+}
